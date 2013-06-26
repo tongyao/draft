ca84b0d177ece0bf74c83411e67560fb385bab1b
tongyao@baidu.com <tongyao@baidu.com>
2013-06-26 15:53:09 +0800

    add mbaas

---
+++ mbaas/mbaas.js
@@ -0,0 +1,9 @@
+/**
+ * Do NOT Remove this file.
+ * Could use this file as a debug version of sdk
+ * also the build script calculates the dependencies based on this file.
+ * @author tongyao@baidu.com 
+ */
+
+document.write('<script src="src/mbaas.js">\x3C/script>');
+document.write('<script src="src/jsonp.js">\x3C/script>');
---
+++ mbaas/src/mbaas.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ * Baidu mbaas Javascript SDK
+ * Copyright 2013 Baidu Inc. All rights reserved.
+ * 
+ * @author tongyao(tongyao@baidu.com)
+ * 
+ */
+
+var baidu = baidu || {};
+
+baidu.mbaas = {};
+
+(function(namespace_){
+    
+    baidu.mbaas.version = "1.0.0";
+    
+    
+    
+})(baidu.mbaas);
+
+
+if (typeof module !== 'undefined' && module.exports) {
+    
+} else {
+    
+}
